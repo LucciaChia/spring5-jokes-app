d0433cfc339f5df575d53e3b20b63405a3e37d72
LucciaChia <krausovalucia23@gmail.com>
2019-05-20 09:50:37 +0200

    First commit

---
+++ src/main/java/guru/springframewrok/jokewebapp/JokeWebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframewrok.jokewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeWebappApplication.class, args);
+	}
+
+}
